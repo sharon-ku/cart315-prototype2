90d299d58ed7e237da20a3c14ae275dc743652b3
sharon-ku <71299609+sharon-ku@users.noreply.github.com>
2022-02-17 15:53:59 -0500

    Prot2: removed unused code; deleted houseRoots; added EndWall

--- Assets/CapsizedText.cs
+++ Assets/CapsizedText.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class CapsizedText : MonoBehaviour
 {
-    // public bool boatCapsized = false;
     public Text myText;
 
     // Start is called before the first frame update
